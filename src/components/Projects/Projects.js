d871562a37594f6f10ded2cd76eeb52021de2265
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-09-27 18:13:10 +0100

    updated project titles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
@@ -12,14 +28,14 @@ const Projects = () => (
       {projects.map((p, i) => {
         return (
           <BlogCard key={i}>
-          <Img src={p.image} />
+            <Img src={p.image} />
             <TitleContent>
               <HeaderThree title>{p.title}</HeaderThree>
               <Hr />
             </TitleContent>
             <CardInfo className="card-info">{p.description}</CardInfo>
             <div>
-              <TitleContent>Stack</TitleContent>
+              <TitleContent style={{ marginTop: "20px" }}>Stack</TitleContent>
               <TagList>
                 {p.tags.map((t, i) => {
                   return <Tag key={i}>{t}</Tag>;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,45 +1,49 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Personal Portfolio",
+    description:
+      "Using React, Next.js and Node, I built my responsive personal portfolio website which offers an insight into my skills and experience at a glance. This portfolio website was created as a Progressive Web App(PWA).",
+    image: "/images/1.jpg",
+    tags: ["React", "Next.js", "PWA", "Node"],
+    source: "https://github.com/Lilian-Chinelo/Personal-Portfolio",
+    visit: "https://github.com/Lilian-Chinelo/Personal-Portfolio",
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "Unichat App",
+    description:
+      "Unichat is a Realtime Chat Application that offers flexible chat options for friends, family, and work teams. This Unichat application offers the ability to create chat groups and a notification system to let you know of a new chat message plus much more.",
+    image: "/images/2.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://github.com/Lilian-Chinelo/Unichat-Application",
+    visit: "https://unichatjs.netlify.app",
     id: 1,
   },
   {
-    title: 'TinDog App',
-    description: "This is This is Tinder but for dogs, give your fur babies a chance to meet other cute dogs in the area.",
-    image: '/images/tindog.png',
-    tags: ['HTML', 'CSS'],
-    source: 'https://github.com/Lilian-Chinelo/Start-up-website',
-    visit: 'https://lilian-chinelo.github.io/Start-up-website/',
+    title: "To-Do-List App",
+    description:
+      "This To-do list app project was created to allow users to make different lists (school, home, work, etc) without having to lift a finger.",
+    image: "/images/3.jpg",
+    tags: ["JavaScript", "HTML", "CSS", "Node", "MongoDB", "Express"],
+    source: "https://github.com/Lilian-Chinelo/To-Do-List-web-app",
+    visit: "https://tinyurl.com/3mrsr6xm",
     id: 2,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: "TinDog App",
+    description:
+      "This is Tinder but for dogs, the tindog web app offers dog owners the opportunity to help their dogs find love with other dogs in the area.",
+    image: "/images/4.jpg",
+    tags: ["HTML", "CSS"],
+    source: "https://github.com/Lilian-Chinelo/Start-up-website",
+    visit: "https://lilian-chinelo.github.io/Start-up-website/",
     id: 3,
   },
 ];
 
 export const TimeLineData = [
-  
-  { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: 'Worked as a freelance developer', },
-  { year: 2021, text: 'Continuing to work as a freelancer and contributing to open source projects.', },
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Worked as a freelance developer" },
+  { year: 2021, text: "Continuing to work as a freelancer and contributing to open source projects.",
+  }
 ];
